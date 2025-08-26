16ea121541265742acc34364c38cbc694ebc24d2
Paulo Holanda <paulorfholanda@gmail.com>
2025-08-26 18:47:25 -0300

    :sparkles: teste de velocidade

--- scenes/bb.gd
+++ scenes/bb.gd
@@ -1,6 +1,11 @@
 extends RigidBody3D
 
+@export var backdrag = 10
 
-
-func _ready():
+func _process(delta: float) -> void:
+	#var new_force = sqrt(linear_velocity.z) * backdrag
+	#linear_velocity.y += lerp(linear_velocity.y, new_force, delta*5)
+	linear_velocity.y += backdrag
+	backdrag -= 0.1
+	linear_velocity.y -= 9.8 # gravidade
 	pass
